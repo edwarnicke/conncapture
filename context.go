58e86a3796568b349061f70665345273d23ea94e
Ed Warnicke <hagbard@gmail.com>
2020-07-09 16:33:48 -0500

    Add FromContext to extract the net.Conn on the server side.
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

---
+++ context.go
@@ -0,0 +1,38 @@
+// Copyright (c) 2020 Cisco and/or its affiliates.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package conncapture
+
+import (
+	"context"
+	"net"
+
+	"google.golang.org/grpc/peer"
+)
+
+// FromContext - retrieve net.Conn from context.  Only works inside a 'server' and only works when used in
+// conjunction with conncapture.TransportCredentials(...)
+func FromContext(ctx context.Context) (net.Conn, bool) {
+	p, ok := peer.FromContext(ctx)
+	if !ok || p == nil || p.Addr == nil {
+		return nil, false
+	}
+	conn, ok := p.Addr.(net.Conn)
+	if !ok {
+		return nil, ok
+	}
+	return conn, true
+}
